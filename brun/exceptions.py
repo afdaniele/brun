8381fcf4fd94b189a97c733579fa5c968c71c16f
Andrea F. Daniele <afdaniele@ttic.edu>
2020-01-09 16:41:54 -0600

    combination of fields now uses networkx

--- brun/exceptions.py
+++ brun/exceptions.py
@@ -7,3 +7,9 @@ class TaskFailureError(RuntimeError):
     def __init__(self, msg, result=None):
         self.result = result
         super(RuntimeError, self).__init__(msg)
+
+
+class InvalidConfigurationError(RuntimeError):
+    def __init__(self, msg, data=None):
+        self.data = data
+        super(RuntimeError, self).__init__(msg)
